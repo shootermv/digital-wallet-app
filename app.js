e875306c9d361b7041815040e28d1ef6ad475b5e
moshe vilner <shootermv@gmail.com>
2020-03-24 02:17:13 +0200

    most of server logic refactored to 'app' file

---
+++ app.js
@@ -0,0 +1,29 @@
+﻿require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
+const Seeder = require('./_helpers/seed/Seeder');
+
+// middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(errorHandler);// global error handler
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+app.use('/api/users', require('./users/users.controller'));
+
+// seeding stuff
+if (process.env.SEED === 'true') {
+    const seed = new Seeder();
+    seed.seedDB()
+}
+
+// export
+module.exports = app;
--- server.js
+++ server.js
@@ -1,32 +1,7 @@
-﻿require('rootpath')();
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const jwt = require('_helpers/jwt');
-const errorHandler = require('_helpers/error-handler');
-const Seeder = require('./_helpers/seed/Seeder');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
-
-// use JWT auth to secure the api
-app.use(jwt());
-
-// api routes
-app.use('/api/users', require('./users/users.controller'));
-
-
-if (process.env.SEED === 'true') {
-    const seed = new Seeder();
-    seed.seedDB()
-}
-
-// global error handler
-app.use(errorHandler);
+const app = require('./app');
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3000;
-const server = app.listen(port, function () {
+app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
